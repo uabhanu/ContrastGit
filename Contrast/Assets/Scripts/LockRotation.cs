511209bde74316c06013bbb8420f02428283e550
Rui Goncalves <ruigonc27@gmail.com>
2017-06-10 15:32:22 +0100

    Test Level +1st Test Build

---
+++ Contrast/Assets/Scripts/LockRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockRotation : MonoBehaviour {
+    private Quaternion _OriginalRotation;
+
+	// Use this for initialization
+	void Start () {
+        _OriginalRotation = this.transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       // this.transform.rotation = _OriginalRotation;
+	}
+
+    private void FixedUpdate () {
+        this.transform.rotation = _OriginalRotation;
+    }
+}
