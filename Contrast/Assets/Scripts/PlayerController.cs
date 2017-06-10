348d0332a8709095243efabb1108a86b6f97ebbe
Bhanu <uabhanu@gmail.com>
2017-06-10 12:42:46 +0100

    Level Building WIP

---
+++ Contrast/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float m_moveSpeed;
+    float m_horizontalDir , m_verticalDir;
+
+
+    void Start()
+    {
+		
+	}
+	
+	void Update()
+    {
+		if(Time.timeScale == 0)
+        {
+            return;
+        }
+
+        m_horizontalDir = m_moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        m_verticalDir = m_moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+
+        transform.Translate(m_horizontalDir , 0f , m_verticalDir);
+	}
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag.Equals("Obstacle"))
+        {
+            Debug.Log("Collision");
+        }
+    }
+}
