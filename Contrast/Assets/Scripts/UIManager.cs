d443234fc2d37e74e221a28586c9842b73f64c65
Bhanu <uabhanu@gmail.com>
2017-06-11 10:09:40 +0100

    Petals UI Prefab created

---
+++ Contrast/Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] Image[] petalImages;
+    [SerializeField] int petalsCount;
+
+    void Start()
+    {
+        
+    }
+	
+	void Update()
+    {
+        if(Time.timeScale == 0)
+        {
+            return;
+        }
+
+        if(petalsCount > 0)
+        {
+            petalImages[petalsCount - 1].enabled = true;
+        }
+	}
+}
